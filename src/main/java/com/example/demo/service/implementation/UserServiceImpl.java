95d1508c30fc5dc1a1732cd3d9f11bc5e4645f80
bala-amypo <bala@amypo.in>
2025-12-23 02:21:19 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/UserServiceImpl.java
@@ -1,32 +1,32 @@
-// package com.example.demo.service.implementation;
+package com.example.demo.service.implementation;
 
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.UserService;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-// @Service
-// public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
-//     private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-//     public UserServiceImpl(UserRepository userRepository) {
-//         this.userRepository = userRepository;
-//     }
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-//     @Override
-//     public User saveUser(User user) {
-//         return userRepository.save(user);
-//     }
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 
-//     @Override
-//     public User getUserById(Long id) {
-//         return userRepository.findById(id).orElse(null);
-//     }
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 
-//     @Override
-//     public List<User> getAllUsers() {
-//         return userRepository.findAll();
-//     }
-// }
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
