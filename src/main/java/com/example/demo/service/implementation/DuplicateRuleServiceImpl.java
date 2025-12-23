1096ec33b4e12ea133d50652389543eba098999d
bala-amypo <bala@amypo.in>
2025-12-23 02:30:19 +0000

    User commit

--- src/main/java/com/example/demo/service/implementation/DuplicateRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/implementation/DuplicateRuleServiceImpl.java
@@ -1,27 +1,27 @@
-// package com.example.demo.service.implementation;
+package com.example.demo.service.implementation;
 
-// import com.example.demo.model.DuplicateRule;
-// import com.example.demo.repository.DuplicateRuleRepository;
-// import com.example.demo.service.DuplicateRuleService;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
+import com.example.demo.model.DuplicateRule;
+import com.example.demo.repository.DuplicateRuleRepository;
+import com.example.demo.service.DuplicateRuleService;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-// @Service
-// public class DuplicateRuleServiceImpl implements DuplicateRuleService {
+@Service
+public class DuplicateRuleServiceImpl implements DuplicateRuleService {
 
-//     private final DuplicateRuleRepository repository;
+    private final DuplicateRuleRepository repository;
 
-//     public DuplicateRuleServiceImpl(DuplicateRuleRepository repository) {
-//         this.repository = repository;
-//     }
+    public DuplicateRuleServiceImpl(DuplicateRuleRepository repository) {
+        this.repository = repository;
+    }
 
-//     @Override
-//     public DuplicateRule saveRule(DuplicateRule rule) {
-//         return repository.save(rule);
-//     }
+    @Override
+    public DuplicateRule saveRule(DuplicateRule rule) {
+        return repository.save(rule);
+    }
 
-//     @Override
-//     public List<DuplicateRule> getAllRules() {
-//         return repository.findAll();
-//     }
-// }
+    @Override
+    public List<DuplicateRule> getAllRules() {
+        return repository.findAll();
+    }
+}
